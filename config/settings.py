8e14fb8a88e94c45154a4507f1fbd39f8a8df734
Lucas Mesquita de Souza <lucasmesquita58@hotmail.com>
2026-03-13 22:35:20 -0300

    feat: :wrench: adding `pydantic` config file

---
+++ config/settings.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+from typing import Optional
+
+class Settings(BaseSettings):
+    msal_client_id: str
+    msal_client_secret: str
+    msal_authority: str
+    redirect_uri: str
+    frontend_uri: str
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = False
+
+settings = Settings()
