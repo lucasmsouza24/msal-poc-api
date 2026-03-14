fa202897dcd092a635feafadfea054bfb3dcda66
Lucas Mesquita de Souza <lucasmesquita58@hotmail.com>
2026-03-14 18:53:36 -0300

    fix: :pencil2: fix int -> str type on `db_port` param

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     redirect_uri: str
     frontend_uri: str
     db_host: str
-    db_port: int
+    db_port: str
     db_name: str
     db_user: str
     db_password: str
